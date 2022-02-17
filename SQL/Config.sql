4e7ebee2536ba71f49099546f1ab27069974ecc5
Ravi Mendis <mendismeister@gmail.com>
2022-02-17 17:00:00 +0000

    SQL config

---
+++ SQL/Config.sql
@@ -0,0 +1,7 @@
+-- Config
+-- Author: blkbutterfly74
+-- DateCreated: 2/17/2022 4:18:46 PM
+--------------------------------------------------------------
+INSERT or REPLACE INTO Maps (File, Domain, Name, Description, Image, DisableWorldWrap)
+VALUES 
+	('{8c4ba8f6-0500-402b-9ec3-3d3760c491c2}Maps/TiltedAxis.lua', 'Maps:Expansion2Maps', 'Tilted Axis (Fixed)', 'LOC_MAP_TILTED_AXIS_DESCRIPTION', 'Map_TiltedAxis', 1);
